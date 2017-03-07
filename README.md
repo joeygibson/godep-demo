9d1e92bd4157e98b4713f20f249a03462db13f08
Joey Gibson <joey@joeygibson.com>
2017-03-06 22:06:51 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# godep-demo
+
+A demo of using [godep](https://github.com/tools/godep).
+
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	log "github.com/Sirupsen/logrus"
+)
+
+func main() {
+	log.Info("Starting up")
+	log.Info("Important work")
+	log.Info("Done")
+}
