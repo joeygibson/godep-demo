ba438933f3306560ff3b75b6f9650c00e3fd6b0c
Joey Gibson <joey@joeygibson.com>
2017-04-25 12:53:19 -0400

    made reset script quiet

--- reset.sh
+++ reset.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
-rm -rf Godeps
+rm -rf Godeps 
 rm -rf vendor
 rm -f godep-demo
 
 for lib in github.com/Sirupsen github.com/sirupsen github.com/spf13/cobra; do
-  echo "Deleting $lib"
-  set -x
   rm -rf $GOPATH/src/$lib
   rm -rf $GOPATH/pkg/darwin_amd64/$lib
-  set +x
 done
